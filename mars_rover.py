3991286c8c78c1428b47c00e915a39458f4d59c7
Benjamin Johnson <50123967+bnjn@users.noreply.github.com>
2023-02-09 11:42:13 +0000

    fixed problem with generate_grid shallow copying

--- mars_rover.py
+++ mars_rover.py
@@ -3,24 +3,15 @@ class MarsRover:
         self.grid = []
 
     def generate_grid(self, rows, columns):
-        row = [None] * columns
-        self.grid = [row] * rows
+        make_row = lambda: [ None ] * columns
+        self.grid = [ make_row() for i in range(0, rows) ]
 
     def view_grid(self):
         return self.grid
 
     def place_rover(self, row, column, facing):
         if column == 0:
-            self.grid[0][0] = 'N' # this fails (gives us two 'N's in the grid)
-
-            # this version works
-            # self.grid[0] = ['N', None]
-            
-            # this version works
-            # self.grid = [
-            #     ['N', None],
-            #     [None, None]
-            # ]
+            self.grid[0][0] = 'N'
         else:
             self.grid = [
                 [None, None],
